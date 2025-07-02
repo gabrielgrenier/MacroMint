42e08dd0e5d96bc758b920f86776ddd13a481b50
GabrielGrenier <gabriel.braun-grenier@hotmail.com>
2025-07-01 23:23:39 -0400

    Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,20 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='grid grid-cols-2'>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo mx-auto" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react mx-auto" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='text-center'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='text-red-400'>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
