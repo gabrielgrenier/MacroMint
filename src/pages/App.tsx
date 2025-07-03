110bf5b384fc3d71dba19744e7939daf84cffa82
GabrielGrenier <gabriel.braun-grenier@hotmail.com>
2025-07-02 23:18:15 -0400

    Google fonts + icons + start work on main page layout

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+function App() {
+  // const [hello, setHello] = useState('test');
+
+  return <div className='px-5 py-3'>
+        <h3 className='text-2xl font-bold text-gray-400'>Calories</h3>
+        <div className='bg-white shadow-2xl rounded-3xl px-4 py-2'>
+          asdasd
+        </div>
+  </div>
+}
+
+export default App
