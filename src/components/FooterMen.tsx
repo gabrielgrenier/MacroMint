5bbce41235acd0b3dd252998732d161d2428012b
GabrielGrenier <gabriel.braun-grenier@hotmail.com>
2025-07-06 23:12:17 -0400

    Footer menu + navigation logic

---
+++ src/components/FooterMen.tsx
@@ -0,0 +1,45 @@
+type FooterMenProps = {
+    selectedTab: string;
+    onTabChange: (tab: string) => void;
+};
+
+// TODO: Macros is not a great page name for what it will be, we should rename it later
+function FooterMenu({selectedTab, onTabChange}: FooterMenProps) {
+
+    const selectedClass = (tabName: string) => 
+        tabName === selectedTab ?
+            "text-green-500 font-semibold" : "text-gray-500"; 
+
+    return <div className="fixed bottom-0 left-0 right-0 bg-white border-t py-2 border-gray-300">
+        <div className="grid grid-cols-3 text-center">
+            <div className={selectedClass('macros')} onClick={() => onTabChange('macros')}>
+                <div className="text-2xl leading-none transition duration-300">
+                    <div className="material-symbols-outlined">
+                        scale
+                    </div>
+                </div>
+                <p className="-mt-1">Macros</p>
+            </div>
+
+            <div className={selectedClass('meals')} onClick={() => onTabChange('meals')}>
+                <div className="text-2xl leading-none transition duration-300">
+                    <div className="material-symbols-outlined">
+                        cookie
+                    </div>
+                </div>
+                <p className="-mt-1">Meals</p>
+            </div>
+
+            <div className={selectedClass('me')} onClick={() => onTabChange('me')}>
+                <div className="text-2xl leading-none transition duration-300">
+                    <div className="material-symbols-outlined">
+                        account_circle
+                    </div>
+                </div>
+                <p className="-mt-1">Me</p>
+            </div>
+        </div>
+    </div>
+}
+
+export default FooterMenu;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,19 +1,32 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import DatePicker from "../components/DatePicker"
+import FooterMenu from "../components/FooterMen"
 
 function App() {
-  // const [hello, setHello] = useState('test');
+   const [selectedTab, setSelectedTab] = useState('macros');
 
   return <>
     <DatePicker 
       onAddClick={() => alert("not yet implemented")}
       onCalendarClick={() => alert("not yet implemented")}
     />
-    <div className='px-5 py-3'>
-        <div className='bg-white shadow-2xl rounded-3xl px-4 py-2'>
-          Data blah blah
-        </div>
+
+    <div className="px-5 py-3">
+        {selectedTab === 'macros' &&<div className='bg-white shadow-2xl rounded-3xl px-4 py-2'>
+          Macros blah blah
+        </div>}
+
+        {selectedTab === 'meals' &&<div className='bg-white shadow-2xl rounded-3xl px-4 py-2'>
+          Meals blah blah
+        </div>}
+
+        {selectedTab === 'me' &&<div className='bg-white shadow-2xl rounded-3xl px-4 py-2'>
+          Me blah blah
+        </div>}
+
     </div>
+
+    <FooterMenu selectedTab={selectedTab} onTabChange={setSelectedTab}/>
   </>
 }
 
